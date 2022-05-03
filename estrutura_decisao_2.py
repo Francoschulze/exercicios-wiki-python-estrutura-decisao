80acf7d5a0ca59a782ff779fd528e1275c00dce7
Franco <francoschulze@gmail.com>
2022-05-03 18:16:38 -0300

    Execício 1

---
+++ estrutura_decisao_2.py
@@ -0,0 +1,16 @@
+"""
+Faça um Programa que peça um valor
+e mostre na tela se o valor é positivo ou negativo.
+"""
+
+valor = int(input('Digite um número: '))
+
+if valor > 0:
+    print(f'O valor digitado foi {valor} e o número é positivo! ')
+elif valor == 0:
+    print(f'O valor digitado foi {valor} esse número é neutro! ')
+elif valor < 0:
+    print(f'O valor digitado foi {valor} e o valor é negativo! ')
+else:
+    print('Opção inválida! ')
+
