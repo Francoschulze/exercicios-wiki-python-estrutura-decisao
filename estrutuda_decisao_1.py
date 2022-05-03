ac7edc981673c2e3b29c48c777cbb0d4132c5ace
Franco <francoschulze@gmail.com>
2022-05-03 14:21:23 -0300

    Execício 1

--- estrutuda_decisao_1.py
+++ estrutuda_decisao_1.py
@@ -1,3 +1,11 @@
 """
 Faça um Programa que peça dois números e imprima o maior deles.
 """
+
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+
+if num1 > num2:
+    print(f'O número {num1} é o maior digitado! ')
+else:
+    print(f'O número {num2} é o maior digitado! ')
