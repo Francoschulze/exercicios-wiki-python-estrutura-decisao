95db4e22ca43158ea41a6e16d51bc0abe7e7938e
Franco <francoschulze@gmail.com>
2022-05-04 19:42:23 -0300

    Execício 6

---
+++ estrutura_decisao_6.py
@@ -0,0 +1,14 @@
+"""
+Faça um Programa que leia três números e mostre o maior deles.
+"""
+
+num_1 = int(input('Digite o primeiro número: '))
+num_2 = int(input('Digite o segundo número: '))
+num_3 = int(input('Digite o terceiro número: '))
+
+if num_1 > num_2 and num_1 > num_2 and num_1 > num_3:
+    print(f'O número {num_1} é o maior dentre os três! ')
+elif num_2 > num_1 and num_2 > num_3:
+    print(f'O número {num_2} é o maior dentre os três! ')
+elif num_3 > num_1 and num_3 > num_2:
+    print(f'O número {num_3} é o maior dentre os três! ')
