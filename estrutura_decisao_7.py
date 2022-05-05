dbd2289a4de0d49eeeb4153af228bd5310889fcf
Franco <francoschulze@gmail.com>
2022-05-05 09:14:38 -0300

    Execício 7

--- estrutura_decisao_7.py
+++ estrutura_decisao_7.py
@@ -8,7 +8,7 @@ num_2 = int(input('Digite o segundo número: '))
 num_3 = int(input('Digite o terceiro número: '))
 
 # Maior
-if num_1 > num_2 and num_1 > num_2 and num_1 > num_3:
+if num_1 > num_2 and num_1 > num_3:
     print(f'O número {num_1} é o maior dentre os três! ')
 elif num_2 > num_1 and num_2 > num_3:
     print(f'O número {num_2} é o maior dentre os três! ')
@@ -16,7 +16,7 @@ elif num_3 > num_1 and num_3 > num_2:
     print(f'O número {num_3} é o maior dentre os três! ')
 
 # Menor
-if num_1 < num_2 and num_1 < num_2 and num_1 < num_3:
+if num_1 < num_2 and num_1 < num_3:
     print(f'O número {num_1} é o menor dentre os três! ')
 elif num_2 < num_1 and num_2 < num_3:
     print(f'O número {num_2} é o menor dentre os três! ')
